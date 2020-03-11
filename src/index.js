4e816d40543d78da79c94b75932cb587286c1d12
MrStupid <will.priddle@nelsoncroom.co.uk>
2020-03-11 08:50:42 +0000

    Working on todo list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import TodosContext from "./context";
+import todosReducer from "./reducer";
+
+const App = () => {
+  const initialState = useContext(TodosContext);
+  const [state, dispatch] = useReducer(todosReducer, initialState);
+  return (
+    <TodosContext.Provider value={{ state, dispatch }}></TodosContext.Provider>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
