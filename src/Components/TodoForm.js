d0bdcfb01e58aa184811af7a445d9f00fbb3e82b
Will <w_priddle@hotmail.com>
2020-03-12 17:51:28 +0000

    Possibly some bugs

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,13 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import TodosContext from '../context';
 
 export default function TodoForm() {
     const [todo, setTodo] = useState();
+    const { state: { currentTodo = {}}, dispatch } = useContext(TodosContext);
 
     const handleSubmit = event => {
         event.preventDefault();
-
+        if (currentTodo.text) {
+            dispatch({ type: "UPDATE_TODO", payload: todo });
+        } else {
+            dispatch({ type: "ADD_TODO", payload: todo });
+        }
+        setTodo("");
     }
+
+    useEffect(() => {
+        if (currentTodo.text) {
+            setTodo(currentTodo.text);
+        }
+    }, [currentTodo.id]);
 
     return (
         <form onSubmit={handleSubmit} className="flex justify-center p-5">
@@ -15,6 +27,7 @@ export default function TodoForm() {
                 type="text"
                 className="border-black border-solid border-2"
                 onChange={event => setTodo(event.target.value)}
+                value={todo}
             />
         </form>
     )
